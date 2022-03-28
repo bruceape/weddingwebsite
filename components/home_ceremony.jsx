685f23597066384340951a33f8842aaa74fc1553
Bruce Sullivan <bruceape@gmail.com>
2022-03-28 10:17:51 -0700

    Added address

--- components/home_ceremony.jsx
+++ components/home_ceremony.jsx
@@ -8,7 +8,8 @@ export default function HomeCeremony({ children }) {
 				Ceremony
 			</h3>
 			<ul className={`${styles.times} neue-haas-regular`}>
-				<li>Marin Headlands</li>
+				<li>Battery Mendell</li>
+				<li>Fort Barry, Mendell Rd, Northwest, CA 94950</li>
 				<li><time dateTime="2022-28-05">Saturday, May 28</time></li>
 				<li><time dateTime="15:00">3:00 PM PT</time> / <time dateTime="18:00">6:00 PM ET</time></li>
 			</ul>
